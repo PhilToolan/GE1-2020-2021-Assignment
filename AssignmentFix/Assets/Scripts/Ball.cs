7affb8eafa42854e1686926fbf44be75739b959a
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-12-20 20:20:57 +0000

    implmented menu and pong scene again

---
+++ AssignmentFix/Assets/Scripts/Ball.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 speed; //Ball only moves in 2d space
+    public float speedMultiplier = 1;
+    public AudioSource audioSource;
+    public AudioSource gameMusic;
+    public AudioClip hitSound;
+    public AudioClip endSound;
+    public Animator textAnimator;
+    public Text text;
+    public PongPlayer player;
+
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PongPlayer").GetComponent<PongPlayer>();
+        gameMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver == false)
+        {
+            transform.position += new Vector3(speed.x, speed.y) * Time.deltaTime * speedMultiplier;
+        }
+        
+        //Check if Player missed the ball
+        if (transform.position.x < -9 || transform.position.x > 9 || transform.position.y < -5 || transform.position.y > 5)
+        {
+            End();
+        }
+
+        //Allow user to replay game
+        if (gameOver == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.position = Vector3.zero;
+                gameOver = false;
+                gameMusic.Play();
+                text.text = "";
+            }
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log(col.gameObject.name);
+
+        if (col.gameObject.name == "Paddle Floor")
+        {
+            speed.y = -speed.y; //Send Ball back (Vertical)
+            HitEffect();
+        }
+
+        if (col.gameObject.name == "Paddle Wall")
+        {
+            speed.x = -speed.x; //Send Ball back (Horizontal)
+            HitEffect();
+        }
+    }
+
+    void HitEffect()
+    {
+        textAnimator.SetTrigger("effect");
+        player.totalScore += 1;
+        text.text = player.totalScore.ToString();
+        audioSource.pitch = Random.Range(0.8f, 1.3f);
+        audioSource.PlayOneShot(hitSound);
+    }
+
+    void End()
+    {
+        gameMusic.Stop();
+        transform.position = new Vector3(100, 100, 0);
+        player.totalScore = 0;
+        text.text = "Game Over !\nPress Space to Replay\nEsc to Quit";
+        audioSource.pitch = 1.0f;
+        audioSource.PlayOneShot(hitSound);
+        gameOver = true;
+    }
+}
---
+++ AssignmentFix/Assets/Scripts/PongPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongPlayer : MonoBehaviour
+{
+
+    public Camera camera2D;
+    public LayerMask layerMaskFloor;
+    public LayerMask layerMaskWall;
+    public GameObject paddleFloor;
+    public GameObject paddleWall;
+    public int totalScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotation of "target"
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        Vector3 objectPos = camera2D.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, transform.right, 20, layerMaskFloor);
+        //Debug.DrawRay(transform.position, transform.right * 20, Color.green);
+
+        RaycastHit2D hitWall = Physics2D.Raycast(transform.position, transform.right, 20, layerMaskWall);
+        //Debug.DrawRay(transform.position, transform.right * 20, Color.green);
+
+        //If something was hit, the raycast.collider will not be null
+        if (hitFloor.collider != null)
+        {
+            if (hitFloor.point.y < 0)
+            {
+                paddleFloor.transform.position = new Vector3(hitFloor.point.x, -4.88f);
+            }
+            else //Snap to ceiling
+            {
+                paddleFloor.transform.position = new Vector3(hitFloor.point.x, 4.88f);
+            }
+        }
+
+        //If something was hit, the raycast.collider will not be null
+        if (hitWall.collider != null)
+        {
+            if (hitWall.point.x < 0)
+            {
+                paddleWall.transform.position = new Vector3(-8.68f, hitWall.point.y);
+            }
+            else //Snap to other side
+            {
+                paddleWall.transform.position = new Vector3(8.68f, hitWall.point.y);
+            }
+        }
+    }
+}
