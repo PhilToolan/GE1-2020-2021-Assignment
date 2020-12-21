c2eb0e32577243d1d5a488f90ae5a86fdb18ce8a
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-12-21 20:06:38 +0000

    Setup Club Player

---
+++ AssignmentFix/Assets/Scripts/ClubPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClubPlayer : MonoBehaviour
+{
+    public TunnelSystem tunnelSystem;
+    public float speed;
+
+    private Tunnel currentTunnel;
+
+    void Start ()
+    {
+        //currentTunnel = tunnelSystem.SetupFirstTunnel();
+    }
+}
