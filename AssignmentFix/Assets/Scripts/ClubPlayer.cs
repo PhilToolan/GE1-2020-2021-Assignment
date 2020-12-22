eca64d237be5a2da6bd1eada347b0ae3c774b917
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-12-22 16:36:33 +0000

    Working on enabling and disabling game objects

--- AssignmentFix/Assets/Scripts/ClubPlayer.cs
+++ AssignmentFix/Assets/Scripts/ClubPlayer.cs
@@ -6,10 +6,6 @@ public class ClubPlayer : MonoBehaviour
 {
     public TunnelSystem tunnelSystem;
     public float speed;
-    public Transform target1;
-    public Transform target2;
-    public bool viz1 = true;
-    public float rotSpeed;
 
     private Tunnel currentTunnel;
     private float distanceTraveled;
@@ -17,15 +13,12 @@ public class ClubPlayer : MonoBehaviour
     private float systemRotation;
     private Transform world;
     private float worldRotation;
-    private Vector3 cameraPosition;
 
-    void Start ()
+    void Start()
     {
         world = tunnelSystem.transform.parent;
         currentTunnel = tunnelSystem.SetupFirstTunnel();
-        cameraPosition = this.transform.position;
         SetUpCurrentTunnel();
-        LookAtVisual();
     }
 
     void Update()
@@ -44,10 +37,6 @@ public class ClubPlayer : MonoBehaviour
 
         tunnelSystem.transform.localRotation = Quaternion.Euler(0f, 0f, systemRotation);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            LookAtVisual();
-        }
     }
 
     void SetUpCurrentTunnel()
@@ -63,28 +52,5 @@ public class ClubPlayer : MonoBehaviour
             worldRotation -= 360f;
         }
         world.localRotation = Quaternion.Euler(worldRotation, 0f, 0f);
-    }
-
-    void LookAtVisual()
-    {
-        if (viz1 == true)
-        {
-            viz1 = false;
-            Vector3 toTarget = target1.transform.position - transform.position;
-
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(toTarget), rotSpeed * Time.deltaTime);
-            speed = 0;
-        }
-
-        if (viz1 == false)
-        {
-            viz1 = true;
-            Vector3 toTarget = target2.transform.position - transform.position;
-
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(toTarget), rotSpeed * Time.deltaTime);
-            speed = 5;
-        }
     }
 }
