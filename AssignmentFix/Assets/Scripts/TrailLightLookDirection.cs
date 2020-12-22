514fa51cc695b57826d82c3f156451840f7b0947
PhilToolan <47186301+PhilToolan@users.noreply.github.com>
2020-12-22 15:33:29 +0000

    Tunnel broken again, second visual working for the moment

---
+++ AssignmentFix/Assets/Scripts/TrailLightLookDirection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailLightLookDirection : MonoBehaviour
+{
+
+    public Transform sphere;
+    public float rotSpeed = 180;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toSphere = sphere.transform.position - transform.position;
+
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(toSphere), rotSpeed * Time.deltaTime);
+
+    }
+}
